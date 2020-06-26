c1ebde951b1acdf251d21c5f5559ec87587cf0e3
Daniel Gordi <danial@gordi.ir>
2020-06-27 00:08:57 +0200

    refactor

--- start-vncserver.sh
+++ start-vncserver.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 echo "starting VNC server ..."
-export USER=crawlab
-vncserver :1 -geometry 1280x800 -depth 24 && tail -F /home/crawlab/.vnc/*.log
+export USER=root
+vncserver :1 -geometry 1280x800 -depth 24
+crawlab-server
